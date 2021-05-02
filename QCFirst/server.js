1090b1f34fbe7cc86d2530c25f37a8e73c914b18
David <dav_8612@live.com>
2021-05-02 15:24:42 -0400

    Updating changes to backend

--- QCFirst/server.js
+++ QCFirst/server.js
@@ -17,6 +17,15 @@ app.get('/index.html', function(req, res, next) {
     res.sendFile(__dirname + '/index.html');
 });
 
+//This is how I grabbed the info from the browser.  Check loginpage.html 
+//to see what changes need to be made in that file as well.
+app.post('/loginpage', function(req, res, next) {
+    const email = req.body.email;
+    const pass = req.body.password
+    console.log(email);
+    console.log(pass);
+})
+
 var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port);
 });
