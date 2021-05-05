16d6193a9574ac0f5406cedbeb7a4aba497bf95a
jonathanyulan99 <77367300+jonathanyulan99@users.noreply.github.com>
2021-05-05 14:13:00 -0400

    All PUG Views Updated

--- QCFirst 2.0/server.js
+++ QCFirst 2.0/server.js
@@ -3,7 +3,7 @@ const bodyParser= require('body-parser');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const { mainModule } = require('process');
+const { mainModule, allowedNodeEnvironmentFlags } = require('process');
 
 const signup = require('./routes/signup');
 
@@ -31,8 +31,53 @@ app.get('/index', (req, res, next) => {
     res.render('index');
 });
 
+app.get('/index/instructorhomepage', (req, res, next) => {
+    res.render('instructorhomepage');
+})
+
+app.get('/instructorhomepage', (req, res, next) => {
+    res.render('instructorhomepage');
+})
+
+app.get('/addcourse', (req,res,next) => {
+    res.render('addcourse')
+})
+
+app.get('/createcourse', (req,res,next) => {
+    res.render('createcourse')
+})
+
+app.get('/deletecourse', (req,res,next) => {
+    res.render('deletecourse')
+})
+
+app.get('/dropcourse', (req,res,next) => {
+    res.render('dropcourse')
+})
+
+app.get('/enrollment', (req,res,next) => {
+    res.render('enrollment')
+})
+
+app.get('/loginpage', (req,res,next) => {
+    res.render('loginpage')
+})
+
+app.get('/managecourse', (req,res,next) => {
+    res.render('managecourse')
+})
+
+app.get('/searchresults', (req,res,next) => {
+    res.render('searchresults')
+})
+
+app.get('/signup', (req,res,next) => {
+    res.render('signup')
+})
+
 app.use('/signup', signup);
 app.use('/create', signup);
+
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
